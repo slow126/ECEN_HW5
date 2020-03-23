c9fc58c692856067fee32f49876c12f3cb1e35ad
slow126 <spencerlow126@me.com>
2020-03-23 00:21:06 -0600

    Initial Commit

--- task3.py
+++ task3.py
@@ -12,7 +12,7 @@ FILE_LIST = os.listdir(PATH)
 FILE_LIST.sort()
 
 SHOW_FRAME = False
-STEP = 10
+STEP = 60
 WINDOW = 15
 PT_THRESHOLD = 0.9
 SCALE = 4
@@ -28,20 +28,20 @@ def find_template(next_frame, prev_frame, corners):
     good_corners = []
     for i in range(len(corners)):
         corner = np.squeeze(corners[i])
-        if(corner[0] > WINDOW and corner[1] > WINDOW and corner[1] < next_frame.shape[0] - WINDOW and corner[0] < next_frame.shape[1] - WINDOW):
-            template = prev_frame[int(corner[1] - WINDOW):int(corner[1] + WINDOW), int(corner[0] - WINDOW):int(corner[0] + WINDOW)]
-            # cv2.imshow("template", template)
-            # cv2.waitKey()
-            match = cv2.matchTemplate(next_frame, (template), cv2.TM_CCOEFF_NORMED)
-            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(match)
-            # max_loc = tuple(map(sum, zip(max_loc, (WINDOW, WINDOW))))
-            # found = next_frame[max_loc[1] - WINDOW:max_loc[1] + WINDOW, max_loc[0] - WINDOW :max_loc[0] + WINDOW]
-            # cv2.imshow("found", found)
-            # cv2.waitKey()
-            if max_val > PT_THRESHOLD:
-                max_loc = tuple(map(sum, zip(max_loc, (WINDOW, WINDOW))))
-                nextPts += [max_loc]
-                good_corners += [corner]
+        # if(corner[0] > WINDOW and corner[1] > WINDOW and corner[1] < next_frame.shape[0] - WINDOW and corner[0] < next_frame.shape[1] - WINDOW):
+        template = prev_frame[int(corner[1] - WINDOW):int(corner[1] + WINDOW), int(corner[0] - WINDOW):int(corner[0] + WINDOW)]
+        # cv2.imshow("template", template)
+        # cv2.waitKey()
+        match = cv2.matchTemplate(next_frame, (template), cv2.TM_CCOEFF_NORMED)
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(match)
+        # max_loc = tuple(map(sum, zip(max_loc, (WINDOW, WINDOW))))
+        # found = next_frame[max_loc[1] - WINDOW:max_loc[1] + WINDOW, max_loc[0] - WINDOW :max_loc[0] + WINDOW]
+        # cv2.imshow("found", found)
+        # cv2.waitKey()
+        # if max_val > PT_THRESHOLD:
+        max_loc = tuple(map(sum, zip(max_loc, (WINDOW, WINDOW))))
+        nextPts += [max_loc]
+        good_corners += [corner]
     return good_corners, nextPts
 
 
@@ -51,6 +51,7 @@ for j in range(len(FILE_LIST) - STEP):
     frame = cv2.imread(os.path.join(PATH, file))
     frame = cv2.resize(frame, (int(frame.shape[1] / SCALE), int(frame.shape[0] / SCALE)))
     gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    gray = np.pad(gray, ((WINDOW, WINDOW), (WINDOW, WINDOW)))
     corners = cv2.goodFeaturesToTrack(gray, NUM_PTS, .1, 1)
     next_corners = np.zeros_like(corners)
     if SHOW_FRAME:
@@ -60,11 +61,15 @@ for j in range(len(FILE_LIST) - STEP):
         cv2.waitKey()
 
     prev_corners = corners
+    corners = np.squeeze(corners)
+    total_mask = np.ones((50, 1))
+
     for i in range(m):
         next_file = FILE_LIST[j + i + 1]
         next_frame = cv2.imread(os.path.join(PATH, next_file))
         next_frame = cv2.resize(next_frame, (int(next_frame.shape[1] / SCALE), int(next_frame.shape[0] / SCALE)))
         next_gray = cv2.cvtColor(next_frame, cv2.COLOR_RGB2GRAY)
+        next_gray = np.pad(next_gray, ((WINDOW, WINDOW), (WINDOW, WINDOW)))
         prev_corners, next_corners = find_template(next_gray, gray, prev_corners)
         retval, mask = cv2.findFundamentalMat(np.array(prev_corners), np.array(next_corners), cv2.FM_RANSAC, 3, 0.9)
         if mask is None:
@@ -73,7 +78,9 @@ for j in range(len(FILE_LIST) - STEP):
         if len(mask) < 50:
             x = 1
         gray = next_gray
-        prev_corners = next_corners * mask
+        prev_corners = next_corners # * mask
+        # corners = corners * mask
+        total_mask = total_mask * mask
         # if len(mask) < NUM_PTS:
         #     PAD = NUM_PTS - len(mask)
         #     for h in range(PAD):
@@ -82,13 +89,15 @@ for j in range(len(FILE_LIST) - STEP):
         #
 
 
-
+    corners = corners * total_mask
+    prev_corners = prev_corners * total_mask
     print(len(next_corners))
-    corners = np.squeeze(corners)
+    corners = corners - WINDOW
+    prev_corners = prev_corners - WINDOW
     for i in range(len(prev_corners)):
-        if(tuple(corners[i]) != (0,0) and tuple(next_corners[i]) != (0,0)):
-            cv2.line(frame, tuple(np.squeeze(corners[i])), tuple(np.squeeze(prev_corners[i])), (255, 0, 255), thickness=1)
+        if(tuple(corners[i]) != (0,0) and tuple(prev_corners[i]) != (0,0)):
+            cv2.line(frame, tuple(np.squeeze(corners[i]).astype(int)), tuple(np.squeeze(prev_corners[i]).astype(int)), (255, 0, 255), thickness=1)
     cv2.imwrite("inter_frame/frame-step-" + str(STEP).zfill(3) + "-" + str(j).zfill(5) + ".jpg", frame)
-    cv2.imshow("frame", frame)
-    cv2.waitKey(1)
+    # cv2.imshow("frame", frame)
+    # cv2.waitKey(1)
     x = 1
